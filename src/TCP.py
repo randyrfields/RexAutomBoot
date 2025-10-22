22545442274c0fbae84572d4473f8323f648b49b
Randy Fields <randyrfields@gmail.com>
2025-10-22 09:52:45 -0400

    remove new code for test

--- src/TCP.py
+++ src/TCP.py
@@ -61,10 +61,10 @@ class TCPEchoDaemon:
             while self.running:
                 try:
                     line = self.receive_line(conn, addr)
-                    # time.sleep(0.25)
+                    time.sleep(0.25)
                     if line != None:
                         decoded = self.decoder.decode_line(line)
-                        self.decoder.sc_format(decoded)
+                        # self.decoder.sc_format(decoded)
                         conn.sendall(line)  # Echo back
                     else:
                         break
